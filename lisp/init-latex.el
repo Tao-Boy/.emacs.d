ad76453ac174420eaf332cb01fd3906f97af4c85
root <root@localhost.localdomain>
2025-12-10 10:56:13 +0800

    update

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -20,34 +20,14 @@
 
 (use-package xenops
   :config
-    (plist-put org-format-latex-options :justify 'right)
-  (defun eli/xenops-justify-fragment-overlay (element &rest _args)
-    (let* ((ov-beg (plist-get element :begin))
-           (ov-end (plist-get element :end))
-           (ov (car (overlays-at (/ (+ ov-beg ov-end) 2) t)))
-           (position (plist-get org-format-latex-options :justify))
-           (inline-p (eq 'inline-math (plist-get element :type)))
-           width offset)
-      (when (and ov
-                 (imagep (overlay-get ov 'display)))
-        (setq width (car (image-display-size (overlay-get ov 'display))))
-        (cond
-         ((and (eq 'center position) 
-               (not inline-p))
-          (setq offset (floor (- (/ fill-column 2)
-                                 (/ width 2))))
-          (if (< offset 0)
-              (setq offset 0))
-          (overlay-put ov 'before-string (make-string offset ? )))
-         ((and (eq 'right position) 
-               (not inline-p))
-          (setq offset (floor (- fill-column
-                                 width)))
-          (if (< offset 0)
-              (setq offset 0))
-          (overlay-put ov 'before-string (make-string offset ? )))))))
-  (advice-add 'xenops-math-display-image :after
-              #'eli/xenops-justify-fragment-overlay))
+  (setq xenops-math-latex-preamble
+        (concat xenops-math-latex-preamble
+                "
+\\usepackage{amsmath,amssymb,physics,bm}
+\\newcommand{\\dif}{\\mathrm{d}}
+")
+  (setq xenops-math-image-scale-factor 1.5))
+
 
 (provide 'init-latex)
 ;; init-latex.el ends here
